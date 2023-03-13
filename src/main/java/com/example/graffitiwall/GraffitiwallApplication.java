5cf7753ae2ee1cfafa8a1585890900c622c5c9be
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-13 12:27:38 +0900

    init

---
+++ src/main/java/com/example/graffitiwall/GraffitiwallApplication.java
@@ -0,0 +1,13 @@
+package com.example.graffitiwall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraffitiwallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraffitiwallApplication.class, args);
+    }
+
+}

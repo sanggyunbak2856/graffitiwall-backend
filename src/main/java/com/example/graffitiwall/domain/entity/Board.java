f48409d826dfdcfad10bb68809aae2ed68f37630
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-13 16:42:32 +0900

    feature : board 엔티티 생성

---
+++ src/main/java/com/example/graffitiwall/domain/entity/Board.java
@@ -0,0 +1,41 @@
+package com.example.graffitiwall.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "board")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Column(name = "category", length = 255, nullable = true)
+    private String category;
+
+    @Column(name = "is_private", nullable = false)
+    private Boolean isPrivate;
+
+    @Column(name = "password", length = 30, nullable = true)
+    private String password;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "board")
+    private List<Postit> postits = new ArrayList<>();
+}

a791cc3de3c36bf60620ae4716a7a79e7eed289d
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-15 12:27:50 +0900

    feature: 보드 서비스 테스트 코그 작성

---
+++ src/test/java/com/example/graffitiwall/web/service/BoardServiceTest.java
@@ -0,0 +1,56 @@
+package com.example.graffitiwall.web.service;
+
+import com.example.graffitiwall.domain.entity.Board;
+import com.example.graffitiwall.domain.repository.BoardRepository;
+import com.example.graffitiwall.web.converter.BoardConverter;
+import com.example.graffitiwall.web.dto.board.BoardPostDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class BoardServiceTest {
+
+    @Mock
+    BoardRepository boardRepository;
+
+    @Spy
+    BoardConverter boardConverter;
+
+    @InjectMocks
+    BoardService boardService;
+
+    @Test
+    @Transactional
+    void BoardService로_보드를_저장한다() {
+        // given
+        BoardConverter boardConverter = new BoardConverter();
+        BoardPostDto boardPostDto = BoardPostDto.builder()
+                .title("hello world")
+                .category("lecture")
+                .isPrivate(false)
+                .build();
+        Board board = boardConverter.boardPostDtoToEntity(boardPostDto);
+        board.setId(1L);
+        board.setCreatedAt(LocalDateTime.now());
+        board.setUpdatedAt(LocalDateTime.now());
+        when(boardRepository.save(any())).thenReturn(board);
+
+        // when
+        Long savedId = boardService.save(boardPostDto);
+
+        // then
+        Assertions.assertThat(savedId).isEqualTo(board.getId());
+    }
+}

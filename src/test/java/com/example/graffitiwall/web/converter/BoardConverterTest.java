e005b49a044ac8601cf88d6f10b39d45b497420a
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-21 15:35:11 +0900

    test : Board -> BoardResponseDto 기능 테스트

--- src/test/java/com/example/graffitiwall/web/converter/BoardConverterTest.java
+++ src/test/java/com/example/graffitiwall/web/converter/BoardConverterTest.java
@@ -1,6 +1,8 @@
 package com.example.graffitiwall.web.converter;
 
 import com.example.graffitiwall.domain.entity.Board;
+import com.example.graffitiwall.domain.entity.User;
+import com.example.graffitiwall.web.dto.board.BoardResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
 import org.junit.jupiter.api.Test;
 
@@ -9,10 +11,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BoardConverterTest {
 
+    BoardConverter boardConverter = new BoardConverter();
+
     @Test
     void 보드_포스트_DTO를_엔티티로_변환한다() {
         // given
-        BoardConverter boardConverter = new BoardConverter();
         BoardSaveDto boardSaveDto = BoardSaveDto.builder()
                 .category("class")
                 .isPrivate(true)
@@ -28,5 +31,32 @@ class BoardConverterTest {
         assertThat(board.getCategory()).isEqualTo(boardSaveDto.getCategory());
         assertThat(board.getPassword()).isEqualTo(boardSaveDto.getPassword());
         assertThat(board.isPrivate()).isEqualTo(boardSaveDto.isPrivate());
+    }
+
+    @Test
+    void 보드_엔티티를_보드_조회_dto로_변환한다() {
+        // given
+        User user = User.builder().build();
+        user.setId(1L);
+        Board board = Board.builder()
+                .user(user)
+                .password("password")
+                .isPrivate(true)
+                .title("board")
+                .category("category")
+                .build();
+        board.setId(1L);
+
+        // when
+        BoardResponseDto boardResponseDto = boardConverter.entityToBoardResponseDto(board);
+
+        // then
+        assertThat(boardResponseDto.getBoardId()).isEqualTo(board.getId());
+        assertThat(boardResponseDto.getCategory()).isEqualTo(board.getCategory());
+        assertThat(boardResponseDto.getPassword()).isEqualTo(board.getPassword());
+        assertThat(boardResponseDto.getTitle()).isEqualTo(board.getTitle());
+        assertThat(boardResponseDto.getCreatedAt()).isEqualTo(board.getCreatedAt());
+        assertThat(boardResponseDto.getUserId()).isEqualTo(board.getUser().getId());
+        assertThat(boardResponseDto.getUpdatedAt()).isEqualTo(board.getUpdatedAt());
     }
 }

f38b832137403f64699dbbb87581e7f1c499fb10
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-21 15:34:59 +0900

    feature : Board -> BoardResponseDto 기능 구현

--- src/main/java/com/example/graffitiwall/web/converter/BoardConverter.java
+++ src/main/java/com/example/graffitiwall/web/converter/BoardConverter.java
@@ -1,6 +1,7 @@
 package com.example.graffitiwall.web.converter;
 
 import com.example.graffitiwall.domain.entity.Board;
+import com.example.graffitiwall.web.dto.board.BoardResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,19 @@ public class BoardConverter {
                 .isPrivate(boardSaveDto.isPrivate())
                 .title(boardSaveDto.getTitle())
                 .password(boardSaveDto.getPassword())
+                .build();
+    }
+
+    public BoardResponseDto entityToBoardResponseDto(Board board) {
+        return BoardResponseDto.builder()
+                .category(board.getCategory())
+                .createdAt(board.getCreatedAt())
+                .isPrivate(board.isPrivate())
+                .password(board.getPassword())
+                .updatedAt(board.getUpdatedAt())
+                .title(board.getTitle())
+                .boardId(board.getId())
+                .userId(board.getUser().getId())
                 .build();
     }
 }

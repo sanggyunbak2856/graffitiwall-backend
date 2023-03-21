9900ce3d890ab84494912eee25b0c414ae15d2de
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-21 15:34:18 +0900

    test : 보드 조회 서비스, 컨트롤러 로직 테스트

--- src/test/java/com/example/graffitiwall/web/controller/BoardControllerTest.java
+++ src/test/java/com/example/graffitiwall/web/controller/BoardControllerTest.java
@@ -1,21 +1,31 @@
 package com.example.graffitiwall.web.controller;
 
+import com.example.graffitiwall.domain.entity.Board;
 import com.example.graffitiwall.domain.entity.User;
 import com.example.graffitiwall.domain.entity.UserStatus;
+import com.example.graffitiwall.domain.repository.BoardRepository;
 import com.example.graffitiwall.domain.repository.UserRepository;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @SpringBootTest
 @AutoConfigureMockMvc
 class BoardControllerTest {
@@ -26,11 +36,18 @@ class BoardControllerTest {
     @Autowired
     private UserRepository userRepository;
 
-    private ObjectMapper objectMapper = new ObjectMapper();
+    @Autowired
+    private BoardRepository boardRepository;
 
-    @Test
-    void 보드_저장_api_성공_테스트() throws Exception {
-        User user = User.builder()
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    User user;
+    User savedUser;
+
+    @BeforeEach
+    void beforeEach() {
+        user = User.builder()
                 .imageUrl("123")
                 .email("efe")
                 .password("grae")
@@ -38,8 +55,11 @@ class BoardControllerTest {
                 .introduce("hello")
                 .userId("userA")
                 .build();
-        User savedUser = userRepository.save(user);
+        savedUser = userRepository.save(user);
+    }
 
+    @Test
+    void 보드_저장_api_성공_테스트() throws Exception {
         BoardSaveDto boardSaveDto = BoardSaveDto.builder()
                 .category("category")
                 .title("title")
@@ -52,6 +72,29 @@ class BoardControllerTest {
                 .content(json))
                 .andExpect(status().isOk())
                 .andDo(print());
+    }
+
+    @Test
+    @Transactional
+    void 보드_조회_테스트() throws Exception {
+        // given
+        Board board = Board.builder()
+                .user(savedUser)
+                .title("new board")
+                .category("category")
+                .isPrivate(true)
+                .password("password")
+                .build();
+        Board savedBoard = boardRepository.save(board);
+
+        // when
+        MvcResult mvcResult = mockMvc.perform(get("/api/v1/boards/" + savedBoard.getId())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+        String contentAsString = mvcResult.getResponse().getContentAsString();
+        log.info(contentAsString);
     }
 
 }
--- src/test/java/com/example/graffitiwall/web/service/BoardServiceTest.java
+++ src/test/java/com/example/graffitiwall/web/service/BoardServiceTest.java
@@ -2,11 +2,14 @@ package com.example.graffitiwall.web.service;
 
 import com.example.graffitiwall.domain.entity.Board;
 import com.example.graffitiwall.domain.entity.User;
+import com.example.graffitiwall.domain.entity.UserStatus;
 import com.example.graffitiwall.domain.repository.BoardRepository;
 import com.example.graffitiwall.domain.repository.UserRepository;
 import com.example.graffitiwall.web.converter.BoardConverter;
+import com.example.graffitiwall.web.dto.board.BoardResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -18,6 +21,8 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,12 +40,22 @@ class BoardServiceTest {
     @InjectMocks
     BoardService boardService;
 
+    User user;
+    @BeforeEach
+    void beforeEach() {
+        user = User.builder()
+                .introduce("hello world")
+                .email("email@email.email")
+                .status(UserStatus.ACTIVE)
+                .userId("userA")
+                .password("password")
+                .build();
+        user.setId(1L);
+    }
+
     @Test
-    @Transactional
     void BoardService로_보드를_저장한다() {
         // given
-        User user = User.builder().build();
-        user.setId(1L);
         BoardSaveDto boardSaveDto = BoardSaveDto.builder()
                 .title("hello world")
                 .category("lecture")
@@ -59,6 +74,23 @@ class BoardServiceTest {
         Long savedId = boardService.save(boardSaveDto);
 
         // then
-        Assertions.assertThat(savedId).isEqualTo(board.getId());
+        assertThat(savedId).isEqualTo(board.getId());
+    }
+
+    @Test
+    void 보드_서비스로_보드를_조회한다() {
+        // given
+        Board board = Board.builder().build();
+        board.setUser(user);
+        board.setId(1L);
+        when(boardRepository.findById(any())).thenReturn(Optional.of(board));
+
+        // when
+        BoardResponseDto responseDto = boardService.findById(1L);
+
+        // then
+        then(boardRepository).should().findById(any());
+        then(boardConverter).should().entityToBoardResponseDto(any());
+        assertThat(responseDto.getBoardId()).isEqualTo(board.getId());
     }
 }

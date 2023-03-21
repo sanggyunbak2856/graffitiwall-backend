230fa55cd43b10c369ac49f0a21c1a952e2f9497
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-21 15:34:35 +0900

    feature : 보드 조회 dto

--- src/main/java/com/example/graffitiwall/web/dto/board/BoardResponseDto.java
+++ src/main/java/com/example/graffitiwall/web/dto/board/BoardResponseDto.java
@@ -1,4 +1,19 @@
 package com.example.graffitiwall.web.dto.board;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
 public class BoardResponseDto {
+    private String title;
+    private String category;
+    private boolean isPrivate;
+    private String password;
+    private Long userId;
+    private Long boardId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }

18c7f000078fe3b5975693d66806f3aebc4603e4
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 12:54:15 +0900

    test : 보드 서비 업데이트 기능 테스트

--- src/test/java/com/example/graffitiwall/web/service/BoardServiceTest.java
+++ src/test/java/com/example/graffitiwall/web/service/BoardServiceTest.java
@@ -5,9 +5,12 @@ import com.example.graffitiwall.domain.entity.User;
 import com.example.graffitiwall.domain.entity.UserStatus;
 import com.example.graffitiwall.domain.repository.BoardRepository;
 import com.example.graffitiwall.domain.repository.UserRepository;
+import com.example.graffitiwall.factory.DummyObjectFactory;
 import com.example.graffitiwall.web.converter.BoardConverter;
+import com.example.graffitiwall.web.dto.IdResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
+import com.example.graffitiwall.web.dto.board.BoardUpdateDto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -21,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static com.example.graffitiwall.factory.DummyObjectFactory.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
@@ -43,29 +47,16 @@ class BoardServiceTest {
     User user;
     @BeforeEach
     void beforeEach() {
-        user = User.builder()
-                .introduce("hello world")
-                .email("email@email.email")
-                .status(UserStatus.ACTIVE)
-                .userId("userA")
-                .password("password")
-                .build();
+        user = createFakeUser();
         user.setId(1L);
     }
 
     @Test
     void BoardService로_보드를_저장한다() {
         // given
-        BoardSaveDto boardSaveDto = BoardSaveDto.builder()
-                .title("hello world")
-                .category("lecture")
-                .isPrivate(false)
-                .userId(1L)
-                .build();
+        BoardSaveDto boardSaveDto = createFakeBoardSaveDto(user.getId());
         Board board = boardConverter.boardPostDtoToEntity(boardSaveDto);
         board.setId(1L);
-        board.setCreatedAt(LocalDateTime.now());
-        board.setUpdatedAt(LocalDateTime.now());
         board.setUser(user);
         when(boardRepository.save(any())).thenReturn(board);
         when(userRepository.findById(any())).thenReturn(Optional.of(user));
@@ -75,12 +66,13 @@ class BoardServiceTest {
 
         // then
         assertThat(savedId).isEqualTo(board.getId());
+        then(boardRepository).should().save(any());
     }
 
     @Test
     void 보드_서비스로_보드를_조회한다() {
         // given
-        Board board = Board.builder().build();
+        Board board = DummyObjectFactory.createFakeBoard();
         board.setUser(user);
         board.setId(1L);
         when(boardRepository.findById(any())).thenReturn(Optional.of(board));
@@ -92,5 +84,22 @@ class BoardServiceTest {
         then(boardRepository).should().findById(any());
         then(boardConverter).should().entityToBoardResponseDto(any());
         assertThat(responseDto.getBoardId()).isEqualTo(board.getId());
+    }
+
+    @Test
+    void 보드_서비스로_보드를_수정한다() {
+        // given
+        Board board = createFakeBoard();
+        BoardUpdateDto boardUpdateDto = createFakeBoardUpdateDto();
+        board.setId(1L);
+        when(boardRepository.findById(any())).thenReturn(Optional.of(board));
+
+        // when
+        IdResponseDto updatedId = boardService.update(1L, boardUpdateDto);
+
+        // then
+        then(boardRepository).should().findById(any());
+        then(boardConverter).should().update(any(), any());
+        assertThat(updatedId.getId()).isEqualTo(1L);
     }
 }

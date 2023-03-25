37a8cbba3b551b5a3065c083b862b64011da4eaf
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 12:55:20 +0900

    feature : 보드 업데이트 컨트롤러 코드

--- src/main/java/com/example/graffitiwall/web/controller/BoardController.java
+++ src/main/java/com/example/graffitiwall/web/controller/BoardController.java
@@ -1,7 +1,9 @@
 package com.example.graffitiwall.web.controller;
 
+import com.example.graffitiwall.web.dto.IdResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
+import com.example.graffitiwall.web.dto.board.BoardUpdateDto;
 import com.example.graffitiwall.web.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -21,5 +23,10 @@ public class BoardController {
     @GetMapping("/{boardId}")
     public BoardResponseDto findById(@PathVariable  Long boardId) {
         return boardService.findById(boardId);
+    }
+
+    @PatchMapping("/{boardId}")
+    public IdResponseDto update(@PathVariable Long boardId, @RequestBody BoardUpdateDto updateDto) {
+        return boardService.update(boardId, updateDto);
     }
 }

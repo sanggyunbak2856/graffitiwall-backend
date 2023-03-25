ddbc81536f1f5928e7f437c422c12676185464fd
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 13:48:44 +0900

    refactor : PostitControllerTest에서 DummyObjectFactory를 통해 객체 생성함

--- src/test/java/com/example/graffitiwall/web/controller/PostitControllerTest.java
+++ src/test/java/com/example/graffitiwall/web/controller/PostitControllerTest.java
@@ -1,15 +1,18 @@
 package com.example.graffitiwall.web.controller;
 
+import com.example.graffitiwall.domain.entity.Board;
+import com.example.graffitiwall.domain.entity.Postit;
 import com.example.graffitiwall.domain.entity.User;
-import com.example.graffitiwall.domain.entity.UserStatus;
+import com.example.graffitiwall.domain.repository.BoardRepository;
+import com.example.graffitiwall.domain.repository.PostitRepository;
 import com.example.graffitiwall.domain.repository.UserRepository;
-import com.example.graffitiwall.web.dto.board.BoardSaveDto;
 import com.example.graffitiwall.web.dto.postit.PostitSaveDto;
 import com.example.graffitiwall.web.dto.postit.PostitUpdateDto;
 import com.example.graffitiwall.web.service.BoardService;
 import com.example.graffitiwall.web.service.PostitService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,6 +21,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.example.graffitiwall.factory.DummyObjectFactory.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -40,44 +44,38 @@ class PostitControllerTest {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    BoardRepository boardRepository;
+
+    @Autowired
+    PostitRepository postitRepository;
+
     ObjectMapper objectMapper = new ObjectMapper();
+
+    User user;
+    Board board;
+
+    String url = "/api/v1/postit";
+
+    @BeforeEach
+    void beforeEach() {
+        user = createFakeUser();
+        board = createFakeBoard();
+        board.setUser(user);
+    }
 
     @Test
     @Transactional
     void 포스트잇_컨트롤러로_포스트잇을_저장한다() throws Exception {
         // given
-        User user = User.builder()
-                .imageUrl("123")
-                .email("efe")
-                .password("grae")
-                .status(UserStatus.ACTIVE)
-                .introduce("hello")
-                .userId("userA")
-                .build();
-        userRepository.save(user);
-        BoardSaveDto boardSaveDto = BoardSaveDto.builder()
-                .category("category")
-                .isPrivate(false)
-                .title("hello world")
-                .userId(user.getId())
-                .build();
-        Long boardId = boardService.save(boardSaveDto);
-        PostitSaveDto postitSaveDto = PostitSaveDto.builder()
-                .angle(0)
-                .boardId(boardId)
-                .color("red")
-                .contents("hello world")
-                .positionX(0)
-                .positionY(0)
-                .title("hello world")
-                .boardId(boardId)
-                .userId(user.getId())
-                .build();
+        Long userId = userRepository.save(user).getId();
+        Long boardId = boardRepository.save(board).getId();
+        PostitSaveDto postitSaveDto = createFakePostitSaveDto(boardId, userId);
         String json = objectMapper.writer().writeValueAsString(postitSaveDto);
         log.info(json);
 
         // when, then
-        mockMvc.perform(post("/api/v1/postit")
+        mockMvc.perform(post(url)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json))
                 .andExpect(status().isOk())
@@ -88,37 +86,16 @@ class PostitControllerTest {
     @Transactional
     void 포스트잇_아이디로_포스트잇을_조회한다() throws Exception {
         // given
-        User user = User.builder()
-                .imageUrl("123")
-                .email("efe")
-                .password("grae")
-                .status(UserStatus.ACTIVE)
-                .introduce("hello")
-                .userId("userA")
-                .build();
-        userRepository.save(user);
-        BoardSaveDto boardSaveDto = BoardSaveDto.builder()
-                .category("category")
-                .isPrivate(false)
-                .title("hello world")
-                .userId(user.getId())
-                .build();
-        Long boardId = boardService.save(boardSaveDto);
-        PostitSaveDto postitSaveDto = PostitSaveDto.builder()
-                .angle(0)
-                .boardId(boardId)
-                .color("red")
-                .contents("hello world")
-                .positionX(0)
-                .positionY(0)
-                .title("hello world")
-                .userId(user.getId())
-                .boardId(boardId)
-                .build();
-        Long postitId = postitService.save(postitSaveDto);
+        User savedUser = userRepository.save(user);
+        Board savedBoard = boardRepository.save(board);
+        Postit postit = createFakePostit();
+        postit.setBoard(savedBoard);
+        postit.setUser(user);
+
+        Long postitId = postitRepository.save(postit).getId();
 
         // when, then
-        mockMvc.perform(get("/api/v1/postit/" + postitId))
+        mockMvc.perform(get(url + "/" + postitId))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -127,46 +104,18 @@ class PostitControllerTest {
     @Transactional
     void 포스트잇을_수정한다() throws Exception {
         // given
-        User user = User.builder()
-                .imageUrl("123")
-                .email("efe")
-                .password("grae")
-                .status(UserStatus.ACTIVE)
-                .introduce("hello")
-                .userId("userA")
-                .build();
-        userRepository.save(user);
-        BoardSaveDto boardSaveDto = BoardSaveDto.builder()
-                .category("category")
-                .isPrivate(false)
-                .title("hello world")
-                .userId(user.getId())
-                .build();
-        Long boardId = boardService.save(boardSaveDto);
-        PostitSaveDto postitSaveDto = PostitSaveDto.builder()
-                .angle(0)
-                .boardId(boardId)
-                .color("red")
-                .contents("hello world")
-                .positionX(0)
-                .positionY(0)
-                .title("hello world")
-                .boardId(boardId)
-                .userId(user.getId())
-                .build();
-        Long postitId = postitService.save(postitSaveDto);
-        PostitUpdateDto postitUpdateDto = PostitUpdateDto.builder()
-                .angle(100)
-                .color("blue")
-                .positionX(20)
-                .positionY(30)
-                .contents("bye world")
-                .title("bye world")
-                .build();
+        User savedUser = userRepository.save(user);
+        Board savedBoard = boardRepository.save(board);
+        Postit postit = createFakePostit();
+        postit.setUser(savedUser);
+        postit.setBoard(savedBoard);
+        Postit savedPostit = postitRepository.save(postit);
+
+        PostitUpdateDto postitUpdateDto = createFakePostitUpdateDto();
         String json = objectMapper.writer().writeValueAsString(postitUpdateDto);
 
         // when, then
-        mockMvc.perform(patch("/api/v1/postit/" + postitId)
+        mockMvc.perform(patch(url + "/" + savedPostit.getId())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(json)
         ).andExpect(status().isOk())
@@ -177,37 +126,15 @@ class PostitControllerTest {
     @Transactional
     void 포스트잇을_삭제한다() throws Exception {
         // given
-        User user = User.builder()
-                .imageUrl("123")
-                .email("efe")
-                .password("grae")
-                .status(UserStatus.ACTIVE)
-                .introduce("hello")
-                .userId("userA")
-                .build();
-        userRepository.save(user);
-        BoardSaveDto boardSaveDto = BoardSaveDto.builder()
-                .category("category")
-                .isPrivate(false)
-                .title("hello world")
-                .userId(user.getId())
-                .build();
-        Long boardId = boardService.save(boardSaveDto);
-        PostitSaveDto postitSaveDto = PostitSaveDto.builder()
-                .angle(0)
-                .boardId(boardId)
-                .color("red")
-                .contents("hello world")
-                .positionX(0)
-                .positionY(0)
-                .title("hello world")
-                .boardId(boardId)
-                .userId(user.getId())
-                .build();
-        Long postitId = postitService.save(postitSaveDto);
+        User savedUser = userRepository.save(user);
+        Board savedBoard = boardRepository.save(board);
+        Postit postit = createFakePostit();
+        postit.setBoard(savedBoard);
+        postit.setUser(savedUser);
+        Postit savedPostit = postitRepository.save(postit);
 
         // when, then
-        mockMvc.perform(delete("/api/v1/postit/" + postitId))
+        mockMvc.perform(delete(url + "/" + savedPostit.getId()))
                 .andExpect(status().isOk())
                 .andDo(print());
     }

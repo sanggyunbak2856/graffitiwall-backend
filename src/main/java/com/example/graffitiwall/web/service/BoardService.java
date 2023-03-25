fb07c706c900b42fd99a491b3c84f9ef4427fb6f
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 12:54:03 +0900

    feature : 보드 서비스 업데이트 기능 구현

--- src/main/java/com/example/graffitiwall/web/service/BoardService.java
+++ src/main/java/com/example/graffitiwall/web/service/BoardService.java
@@ -5,11 +5,14 @@ import com.example.graffitiwall.domain.entity.User;
 import com.example.graffitiwall.domain.repository.BoardRepository;
 import com.example.graffitiwall.domain.repository.UserRepository;
 import com.example.graffitiwall.web.converter.BoardConverter;
+import com.example.graffitiwall.web.dto.IdResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardResponseDto;
 import com.example.graffitiwall.web.dto.board.BoardSaveDto;
+import com.example.graffitiwall.web.dto.board.BoardUpdateDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -19,6 +22,7 @@ public class BoardService {
     private final UserRepository userRepository;
     private final BoardConverter boardConverter;
 
+    @Transactional
     public Long save(BoardSaveDto boardSaveDto) {
         Board board = boardConverter.boardPostDtoToEntity(boardSaveDto);
         User user = userRepository.findById(boardSaveDto.getUserId()).get();
@@ -27,9 +31,17 @@ public class BoardService {
         return savedBoard.getId();
     }
 
+    @Transactional
     public BoardResponseDto findById(Long id) {
         Board board = boardRepository.findById(id).get();
         return boardConverter.entityToBoardResponseDto(board);
+    }
+
+    @Transactional
+    public IdResponseDto update(Long id, BoardUpdateDto updateDto) throws IllegalArgumentException {
+        Board board = boardRepository.findById(id).get();
+        boardConverter.update(board, updateDto);
+        return IdResponseDto.builder().id(board.getId()).build();
     }
 
 

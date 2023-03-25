924bbd2aa5d08f65e3ee3f3953a0aa0a56a0a641
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 13:57:30 +0900

    refactor : userConverterTest에서 DummyObjectFactory를 통해 객체 생성함

--- src/test/java/com/example/graffitiwall/web/service/UserServiceTest.java
+++ src/test/java/com/example/graffitiwall/web/service/UserServiceTest.java
@@ -1,20 +1,18 @@
 package com.example.graffitiwall.web.service;
 
 import com.example.graffitiwall.domain.entity.User;
-import com.example.graffitiwall.domain.entity.UserStatus;
 import com.example.graffitiwall.domain.repository.UserRepository;
 import com.example.graffitiwall.web.converter.UserConverter;
-import com.example.graffitiwall.web.dto.IdResponseDto;
 import com.example.graffitiwall.web.dto.user.UserSaveDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static com.example.graffitiwall.factory.DummyObjectFactory.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.when;
@@ -24,39 +22,24 @@ class UserServiceTest {
     @Mock
     UserRepository userRepository;
 
-    @Mock
+    @Spy
     UserConverter userConverter;
 
     @InjectMocks
     UserService userService;
-
-    UserSaveDto userSaveDto;
-
-    User userA;
+    User user;
 
     @BeforeEach
     void beforeEach() {
-        userSaveDto = UserSaveDto.builder()
-                .userId("userA")
-                .password("password")
-                .email("email@email.com")
-                .introduce("hello world")
-                .build();
-        userA = User.builder()
-                .userId(userSaveDto.getUserId())
-                .password(userSaveDto.getPassword())
-                .status(UserStatus.ACTIVE)
-                .email(userSaveDto.getEmail())
-                .introduce(userSaveDto.getIntroduce())
-                .build();
+        user = createFakeUser();
     }
 
     @Test
     void 유저를_저장한다() {
         // given
-        userA.setId(1L);
-        when(userRepository.save(any())).thenReturn(userA);
-        when(userConverter.userSaveDtoToEntity(any())).thenReturn(userA);
+        user.setId(1L);
+        when(userRepository.save(any())).thenReturn(user);
+        UserSaveDto userSaveDto = createFakeUserSaveDto();
 
         // when
         userService.save(userSaveDto);

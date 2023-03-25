9e86d0f751b547bbf33f0694c45ea80a992a284b
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 13:30:35 +0900

    refactor : PostitConverterTest에서 DummyObjectFactory를 통해 객체 생성함

--- src/test/java/com/example/graffitiwall/web/converter/PostitConverterTest.java
+++ src/test/java/com/example/graffitiwall/web/converter/PostitConverterTest.java
@@ -3,35 +3,38 @@ package com.example.graffitiwall.web.converter;
 import com.example.graffitiwall.domain.entity.Board;
 import com.example.graffitiwall.domain.entity.Postit;
 import com.example.graffitiwall.domain.entity.User;
+import com.example.graffitiwall.factory.DummyObjectFactory;
 import com.example.graffitiwall.web.dto.postit.PostitResponseDto;
 import com.example.graffitiwall.web.dto.postit.PostitUpdateDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
 
+import static com.example.graffitiwall.factory.DummyObjectFactory.*;
 import static org.assertj.core.api.Assertions.*;
 
 class PostitConverterTest {
 
     PostitConverter postitConverter = new PostitConverter();
 
+    User user;
+    Board board;
+    Postit postit;
+
+    @BeforeEach
+    void beforeEach() {
+        user = createFakeUser();
+        board = createFakeBoard();
+        postit = createFakePostit();
+    }
+
     @Test
     void 포스트잇_엔티티를_포스트잇_응답_dto로_변환() {
         // given
-        User user = User.builder().build();
         user.setId(1L);
-        user.setUserId("userA");
-        Board board = Board.builder().build();
         board.setId(1L);
         board.setUser(user);
-        Postit postit = Postit.builder()
-                .title("postit")
-                .positionY(10)
-                .positionX(10)
-                .contents("hello world")
-                .color("red")
-                .angle(10)
-                .build();
         postit.setId(1L);
         postit.setCreatedAt(LocalDateTime.now());
         postit.setUpdatedAt(LocalDateTime.now());
@@ -59,25 +62,8 @@ class PostitConverterTest {
     @Test
     void 포스트잇을_업데이트한다() {
         // given
-        Board board = Board.builder().build();
         board.setId(1L);
-        Postit postit = Postit.builder()
-                .title("postit")
-                .positionY(10)
-                .positionX(10)
-                .contents("hello world")
-                .color("red")
-                .board(board)
-                .angle(10)
-                .build();
-        PostitUpdateDto postitUpdateDto = PostitUpdateDto.builder()
-                .angle(100)
-                .contents("bye")
-                .color("blue")
-                .positionX(22)
-                .positionY(33)
-                .title("sllsl")
-                .build();
+        PostitUpdateDto postitUpdateDto = createFakePostitUpdateDto();
 
         // when
         postitConverter.postitUpdate(postit, postitUpdateDto);

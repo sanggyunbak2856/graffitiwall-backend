85c2ca3e99a4139cb94f80a72dfe7f938f510173
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-25 13:10:16 +0900

    refactor : 객체를 생성하는 부분을 DummyObjectFactory가 하도록 리팩토링함

--- src/test/java/com/example/graffitiwall/domain/repository/BoardRepositoryTest.java
+++ src/test/java/com/example/graffitiwall/domain/repository/BoardRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.graffitiwall.domain.entity.Board;
 import com.example.graffitiwall.domain.entity.Postit;
 import com.example.graffitiwall.domain.entity.User;
 import com.example.graffitiwall.domain.entity.UserStatus;
+import com.example.graffitiwall.factory.DummyObjectFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
@@ -15,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static com.example.graffitiwall.factory.DummyObjectFactory.*;
 import static org.assertj.core.api.Assertions.*;
 
 @Slf4j
@@ -36,29 +38,9 @@ public class BoardRepositoryTest {
 
     @BeforeEach
     void beforeEach() {
-        user = User.builder()
-                .status(UserStatus.ACTIVE)
-                .password("p")
-                .email("email@email.c")
-                .imageUrl("po.p")
-                .userId("userA")
-                .build();
-        board = Board.builder()
-                .category("category")
-                .isPrivate(false)
-                .title("hello")
-                .user(user)
-                .build();
-        postit = Postit.builder()
-                .title("postit")
-                .positionY(10)
-                .positionX(10)
-                .contents("hello world")
-                .color("red")
-                .board(board)
-                .user(user)
-                .angle(10)
-                .build();
+        user = createFakeUser();
+        board = createFakeBoard();
+        postit = createFakePostit();
     }
 
     @AfterEach
@@ -101,33 +83,9 @@ public class BoardRepositoryTest {
     @Transactional
     void 디비에서_보드를_삭제시_보드에_있던_포스트잇들이_삭제된다() {
         // given
-        Board board = Board.builder()
-                .title("hello board")
-                .isPrivate(false)
-                .category("category")
-                .user(user)
-                .build();
         Board savedBoard = boardRepository.save(board);
-        Postit postIt1 = Postit.builder()
-                .title("postit")
-                .positionY(10)
-                .positionX(10)
-                .contents("hello world")
-                .color("red")
-                .board(board)
-                .user(user)
-                .angle(10)
-                .build();
-        Postit postIt2 = Postit.builder()
-                .title("hello")
-                .positionY(10)
-                .positionX(10)
-                .contents("bye world")
-                .color("blue")
-                .board(board)
-                .user(user)
-                .angle(10)
-                .build();
+        Postit postIt1 = createFakePostit();
+        Postit postIt2 = createFakePostit();
         Postit savedPostit1 = postitRepository.save(postIt1);
         Postit savedPostit2 = postitRepository.save(postIt2);
         board.getPostits().add(savedPostit1);
@@ -150,11 +108,6 @@ public class BoardRepositoryTest {
     @Transactional
     void 디비에서_보드를_조회한다() {
         // given
-        Board board = Board.builder()
-                .title("hello board")
-                .isPrivate(false)
-                .category("category")
-                .build();
         Board savedBoard = boardRepository.save(board);
 
         // when
@@ -168,11 +121,7 @@ public class BoardRepositoryTest {
     @Transactional
     void 디비에서_보드를_수정한다() {
         // given
-        Board board = Board.builder()
-                .title("hello board")
-                .isPrivate(false)
-                .category("category")
-                .build();
+        Board board = createFakeBoard();
         Board savedBoard = boardRepository.save(board);
 
         // when
@@ -182,34 +131,5 @@ public class BoardRepositoryTest {
         // then
         assertThat(updatedBoard.getTitle()).isEqualTo("bye board");
         assertThat(updatedBoard.getId()).isEqualTo(savedBoard.getId());
-    }
-
-    @Test
-    @Transactional
-    void 보드가_삭제될_때_포스트잇도_함께_삭제된다() {
-        // given
-        userRepository.save(user);
-        Board board = Board.builder()
-                .title("hello board")
-                .isPrivate(false)
-                .category("category")
-                .build();
-        board.setUser(user);
-        Board savedBoard = boardRepository.save(board);
-        Postit postit = Postit.builder()
-                .title("postit")
-                .positionY(10)
-                .positionX(10)
-                .contents("hello world")
-                .color("red")
-                .user(user)
-                .angle(10)
-                .build();
-        postit.setBoard(board);
-        postit.setUser(user);
-        Postit savedPostit = postitRepository.save(postit);
-
-        // when
-        boardRepository.delete(board);
     }
 }

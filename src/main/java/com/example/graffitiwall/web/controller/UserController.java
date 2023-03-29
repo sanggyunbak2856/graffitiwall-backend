4ae78f2e14f4df25066684d28935a5010b8c0b77
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-29 12:46:31 +0900

    feature : 유저 업데이트 기능 컨트롤러, 서비스

--- src/main/java/com/example/graffitiwall/web/controller/UserController.java
+++ src/main/java/com/example/graffitiwall/web/controller/UserController.java
@@ -2,13 +2,11 @@ package com.example.graffitiwall.web.controller;
 
 import com.example.graffitiwall.web.dto.IdResponseDto;
 import com.example.graffitiwall.web.dto.user.UserSaveDto;
+import com.example.graffitiwall.web.dto.user.UserUpdateDto;
 import com.example.graffitiwall.web.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,5 +19,10 @@ public class UserController {
     @PostMapping
     public IdResponseDto register(@RequestBody UserSaveDto userSaveDto) {
         return userService.save(userSaveDto);
+    }
+
+    @PatchMapping("/{userId}")
+    public IdResponseDto update(@PathVariable Long userId, @RequestBody UserUpdateDto userUpdateDto) {
+        return userService.update(userId, userUpdateDto);
     }
 }
--- src/main/java/com/example/graffitiwall/web/service/UserService.java
+++ src/main/java/com/example/graffitiwall/web/service/UserService.java
@@ -5,8 +5,10 @@ import com.example.graffitiwall.domain.repository.UserRepository;
 import com.example.graffitiwall.web.converter.UserConverter;
 import com.example.graffitiwall.web.dto.IdResponseDto;
 import com.example.graffitiwall.web.dto.user.UserSaveDto;
+import com.example.graffitiwall.web.dto.user.UserUpdateDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +16,18 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserConverter userConverter;
 
+    @Transactional
     public IdResponseDto save(UserSaveDto userSaveDto) {
         User savedUser = userRepository.save(userConverter.userSaveDtoToEntity(userSaveDto));
         return IdResponseDto.builder()
                 .id(savedUser.getId())
                 .build();
+    }
+
+    @Transactional
+    public IdResponseDto update(Long id, UserUpdateDto userUpdateDto) {
+        User user = userRepository.findById(id).get();
+        userConverter.userUpdate(user, userUpdateDto);
+        return IdResponseDto.builder().id(user.getId()).build();
     }
 }

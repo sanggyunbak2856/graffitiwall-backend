5aba03b846659e54ae01e57d210807f8dd0eaf14
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-03-30 11:58:06 +0900

    feature : 유저 id로 보드 목록을 조회하는 기능

--- src/main/java/com/example/graffitiwall/domain/repository/BoardRepository.java
+++ src/main/java/com/example/graffitiwall/domain/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package com.example.graffitiwall.domain.repository;
 import com.example.graffitiwall.domain.entity.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findBoardByUser_Id(Long id);
 }

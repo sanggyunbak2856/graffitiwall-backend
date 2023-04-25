8a649a978b9309aeb26cdb02a4786fb2f61ad610
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-04-25 16:01:00 +0900

    feature : 로그인 체크 기능 구현

--- src/main/java/com/example/graffitiwall/config/WebConfig.java
+++ src/main/java/com/example/graffitiwall/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.example.graffitiwall.config;
 
+import com.example.graffitiwall.web.interceptor.BoardLoginCheckInterceptor;
+import com.example.graffitiwall.web.interceptor.PostitLoginCheckInterceptor;
+import com.example.graffitiwall.web.interceptor.UserLoginCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +15,21 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserLoginCheckInterceptor())
+                .order(1)
+                .addPathPatterns("/api/v1/users/**")
+                .excludePathPatterns("/api/v1/users", "/api/v1/users/**/duplicate");
+
+        registry.addInterceptor(new BoardLoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/api/v1/boards/**");
+
+        registry.addInterceptor(new PostitLoginCheckInterceptor())
+                .order(3)
+                .addPathPatterns("/api/v1/postit/**");
     }
 }

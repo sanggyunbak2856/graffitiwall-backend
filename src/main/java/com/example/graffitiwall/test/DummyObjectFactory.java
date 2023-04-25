0e67e22d0c3585e499f695f4318c84e7c8c90c4c
sanggyunbak2856 <baksanggyun2856@gmail.com>
2023-04-25 12:58:29 +0900

    fix : 설정파일 오타 수정

---
+++ src/main/java/com/example/graffitiwall/test/DummyObjectFactory.java
@@ -0,0 +1,125 @@
+package com.example.graffitiwall.test;
+
+import com.example.graffitiwall.domain.entity.Board;
+import com.example.graffitiwall.domain.entity.Postit;
+import com.example.graffitiwall.domain.entity.User;
+import com.example.graffitiwall.domain.entity.UserStatus;
+import com.example.graffitiwall.web.dto.board.BoardSaveDto;
+import com.example.graffitiwall.web.dto.board.BoardUpdateDto;
+import com.example.graffitiwall.web.dto.postit.PostitSaveDto;
+import com.example.graffitiwall.web.dto.postit.PostitUpdateDto;
+import com.example.graffitiwall.web.dto.user.UserSaveDto;
+import com.example.graffitiwall.web.dto.user.UserUpdateDto;
+import com.github.javafaker.Faker;
+
+public class DummyObjectFactory {
+    private static Faker faker = new Faker();
+    private static int TITLE_LENGTH = 100;
+    private static int CONTENTS_LENGTH = 1024;
+
+    public static User createFakeUser() {
+        return User.builder()
+                .password(faker.internet().password())
+                .userId(faker.name().username())
+                .email(faker.internet().emailAddress())
+                .status(UserStatus.ACTIVE)
+                .introduce(faker.lorem().characters(CONTENTS_LENGTH))
+                .imageUrl(faker.internet().url())
+                .nickname(faker.name().username())
+                .build();
+    }
+
+    public static UserSaveDto createFakeUserSaveDto() {
+        return UserSaveDto.builder()
+                .password(faker.internet().password())
+                .userId(faker.name().username())
+                .email(faker.internet().emailAddress())
+                .introduce(faker.lorem().characters())
+                .nickname(faker.name().username())
+                .build();
+    }
+
+    public static UserUpdateDto createFakeUserUpdateDto() {
+        return UserUpdateDto.builder()
+                .password(faker.internet().password())
+                .userId(faker.name().username())
+                .email(faker.internet().emailAddress())
+                .introduce(faker.lorem().characters())
+                .nickname(faker.name().username())
+                .build();
+    }
+
+    public static Board createFakeBoard() {
+        return Board.builder()
+                .password(faker.internet().password())
+                .isPrivate(faker.random().nextBoolean())
+                .category(faker.zelda().character())
+                .title(faker.lorem().characters(TITLE_LENGTH))
+                .build();
+    }
+
+    public static BoardSaveDto createFakeBoardSaveDto(Long userId) {
+        return BoardSaveDto.builder()
+                .password(faker.internet().password())
+                .isPrivate(faker.random().nextBoolean())
+                .category(faker.zelda().character())
+                .title(faker.lorem().characters())
+                .userId(userId)
+                .build();
+    }
+
+    public static BoardUpdateDto createFakeBoardUpdateDto() {
+        return BoardUpdateDto.builder()
+                .password(faker.internet().password())
+                .isPrivate(faker.random().nextBoolean())
+                .category(faker.zelda().character())
+                .title(faker.lorem().characters(TITLE_LENGTH))
+                .build();
+    }
+
+    public static Postit createFakePostit() {
+        return Postit.builder()
+                .title(faker.lorem().characters(TITLE_LENGTH))
+                .contents(faker.lorem().characters(CONTENTS_LENGTH))
+                .positionY(faker.random().nextInt(0, 500))
+                .positionX(faker.random().nextInt(0, 500))
+                .color(faker.color().name())
+                .angle(faker.random().nextInt(0, 360))
+                .font(faker.lorem().characters(10))
+                .sizeX(faker.random().nextInt(0, 100))
+                .sizeY(faker.random().nextInt(0, 100))
+                .build();
+    }
+
+    public static PostitSaveDto createFakePostitSaveDto(Long boardId, Long userId) {
+        return PostitSaveDto.builder()
+                .title(faker.lorem().characters(TITLE_LENGTH))
+                .contents(faker.lorem().characters(CONTENTS_LENGTH))
+                .positionY(faker.random().nextInt(0, 500))
+                .positionX(faker.random().nextInt(0, 500))
+                .color(faker.color().name())
+                .angle(faker.random().nextInt(0, 360))
+                .boardId(boardId)
+                .userId(userId)
+                .font(faker.lorem().characters(10))
+                .sizeX(faker.random().nextInt(0, 100))
+                .sizeY(faker.random().nextInt(0, 100))
+                .build();
+    }
+
+    public static PostitUpdateDto createFakePostitUpdateDto() {
+        return PostitUpdateDto.builder()
+                .title(faker.lorem().characters(TITLE_LENGTH))
+                .contents(faker.lorem().characters(CONTENTS_LENGTH))
+                .positionY(faker.random().nextInt(0, 500))
+                .positionX(faker.random().nextInt(0, 500))
+                .color(faker.color().name())
+                .angle(faker.random().nextInt(0, 360))
+                .font(faker.lorem().characters(10))
+                .sizeX(faker.random().nextInt(0, 100))
+                .sizeY(faker.random().nextInt(0, 100))
+                .build();
+    }
+
+
+}
---
+++ src/main/java/com/example/graffitiwall/test/Test.java
@@ -0,0 +1,36 @@
+package com.example.graffitiwall.test;
+
+import com.example.graffitiwall.domain.entity.Board;
+import com.example.graffitiwall.domain.entity.User;
+import com.example.graffitiwall.domain.repository.BoardRepository;
+import com.example.graffitiwall.domain.repository.UserRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static com.example.graffitiwall.test.DummyObjectFactory.createFakeBoard;
+import static com.example.graffitiwall.test.DummyObjectFactory.createFakeUser;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class Test {
+
+    private final UserRepository userRepository;
+    private final BoardRepository boardRepository;
+
+    @PostConstruct
+    void postConstruct() {
+        log.info("test post construct");
+        User user = createFakeUser();
+        userRepository.save(user);
+
+        for(int i = 0; i < 30; i++) {
+            Board board = createFakeBoard();
+            board.setUser(user);
+            boardRepository.save(board);
+        }
+    }
+}
